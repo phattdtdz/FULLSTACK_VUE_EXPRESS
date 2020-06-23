02f0df63d2467cc0d4088e713002646cb3a08312
Cao Thành Phát <phatct@bap.jp>
2020-06-23 16:19:12 +0700

    Initial backend create

---
+++ server/routes/api/posts.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const mongodb = require ("mongodb");
+
+router = express.Router();
+
+router.get('/a', async(req,res)=>{
+const posts = await loadPostsCollection();
+await res.send(await posts.find({}).toArray())
+
+})
+
+
+router.post('/a',async(req,res)=>{
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+
+    });
+    res.status(201).send()
+})
+
+
+router.delete('/:id', async(req,res)=>{
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)})
+    res.status(200).send();
+})
+
+async function loadPostsCollection(){
+    const client = await mongodb.MongoClient.connect("mongodb+srv://user_admin:Phat1998@cluster0-qnjmt.gcp.mongodb.net", {useNewUrlParser: true});
+    return client.db('vue-express').collection("posts")
+}
+
+
+module.exports = router;
